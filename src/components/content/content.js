fbb8f9be4494e7574dcd8b5426c59bf6277dfe68
Tai. Pham Tinh Hong Tan <taiptht@vng.com.vn>
2018-12-01 15:31:49 +0700

    create sider bar, content of profile page, header

---
+++ src/components/content/content.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './content.css';
+
+class Content extends Component{
+    render(){
+        return(
+            <div className="main-content">
+
+            </div>
+        )
+    }
+}
+export default Content;
---
+++ src/components/sider-right/sider-right.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './sider-right.css';
+
+class SiderRight extends Component{
+    render(){
+        return(
+            <div className="sider-right">
+
+            </div>
+        )
+    }
+}
+
+export default SiderRight;
